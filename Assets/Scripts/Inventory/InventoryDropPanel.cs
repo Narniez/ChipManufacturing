c2624d4ff86309cce1c3f2bbf9e6231a1aabd917
anca6 <526100@student.saxion.nl>
2025-10-09 16:50:13 +0200

    Inventory item draggable into game view

---
+++ Assets/Scripts/Inventory/InventoryDropPanel.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryDropPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject inventoryPanel; // to enable/disable when dragging over drop panel
+    [SerializeField] private Button inventoryButton;
+
+    public bool IsPointerOver { get; private set; }
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        IsPointerOver = true;
+        inventoryPanel.SetActive(false);
+        inventoryButton.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsPointerOver = false;
+        if (inventoryPanel != null)
+        {
+            inventoryButton.gameObject.SetActive(false);
+        }
+    }
+}
