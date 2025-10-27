b85ef53acccba7b2bd9620625f2ab12064f00715
anca6 <526100@student.saxion.nl>
2025-10-27 15:45:22 +0100

    Camera unlocked when exiting inventory

--- Assets/Scripts/Inventory/InventoryDropPanel.cs
+++ Assets/Scripts/Inventory/InventoryDropPanel.cs
@@ -4,15 +4,18 @@ using UnityEngine.UI;
 
 public class InventoryDropPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private CameraController mainCamera;
     [SerializeField] private GameObject inventoryPanel; // to enable/disable when dragging over drop panel
     [SerializeField] private Button inventoryButton;
 
+    private Button _button;
+
     public bool IsPointerOver { get; private set; }
-   
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         IsPointerOver = true;
+        mainCamera.SetInputLocked(false);
         inventoryPanel.SetActive(false);
         inventoryButton.gameObject.SetActive(true);
     }
