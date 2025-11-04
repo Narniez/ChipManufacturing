0e55a879775d9e07e845d91f866b56fc1d0660c8
Narniez <trifon221@gmail.com>
2025-11-04 02:41:54 +0200

    Scene setup for demo

--- Assets/Scripts/Machines/Machine.cs
+++ Assets/Scripts/Machines/Machine.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.VFX;
+using Unity.VisualScripting;
 
 public class Machine : MonoBehaviour, IInteractable, IDraggable, IGridOccupant
 {
